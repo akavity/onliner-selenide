fc8bdd5a7a326f59221e247671fd8cd91ba88f39
Basil Akavity <vakavity@gmail.com>
2023-11-24 22:08:00 +0300

    add ShoppingPage, ShoppingSteps

--- src/main/java/org/example/pages/ShoppingPage.java
+++ src/main/java/org/example/pages/ShoppingPage.java
@@ -8,22 +8,50 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class ShoppingPage {
-     ElementsCollection products = $$(By.xpath(" //div[@id='schema-products']"));
+    ElementsCollection products = $$(By.xpath(" //div[@id='schema-products']"));
 
-     public SelenideElement schemaHeaderText = $(By.xpath("//div[@class='schema-header']/h1"));
+    public SelenideElement schemaHeaderText = $(By.xpath("//div[@class='schema-header']/h1"));
 
-     //span[contains(@data-bind,'.full_name')]     название продуктов;
-     //span[contains(@data-bind,'.minPrice')]      цена продуктов
-     //div[@class="schema-product__status"]  нет в наличии
-     //div[@class="schema-product__offers"] количество предложений
+    public SelenideElement controlElement = $(By.xpath("//div[contains(@class, 'schema-filter-button__inner-container')]"));
 
+    public SelenideElement controlButton = $(By.xpath("//span[contains(@class, 'button__sub_control')]"));
 
-     //div[contains(@class, "schema-product")]
+    public SelenideElement schemaFilterCheckboxItem(String label, String checkBoxItemName) {
+        return $(By.xpath("//span[contains(text(), '" + label + "')]" +
+                "//..//../div[contains(@class, 'facet')]//li//span[contains(text(), '" + checkBoxItemName + "')]/.."));
+    }
 
+    public SelenideElement schemaFilterDropDown(String label) {
+        return $(By.xpath("//span[contains(text(), '" + label + "')]" +
+                "//..//../div[contains(@class, 'facet')]/div[contains(@class,'control')]"));
+    }
 
-     public SelenideElement getSchemaHeadText() {
-          return schemaHeaderText;
-     }
+    public SelenideElement popoverVisibleItem(String label, String itemName) {
+        return $(By.xpath("//span[contains(text(), '" + label + "')]" +
+                "/../following-sibling::div//span[contains(text(), '" + itemName + "')]"));
+    }
 
+    public SelenideElement minValueField(String label) {
+        return $(By.xpath("//span[contains(text(), '" + label + "')]" +
+                "/..//../div[contains(@class, 'facet')]//div[contains(@class, 'control_input')][1]"));
+    }
+
+    public SelenideElement maxValueField(String label) {
+        return $(By.xpath("//span[contains(text(), '" + label + "')]" +
+                "/..//../div[contains(@class, 'facet')]//div[contains(@class, 'control_input')][2]"));
+    }
+
+    public SelenideElement schemaFilterButton() {
+        return $(By.xpath("//span[contains(@class, 'n__sub_m')]"));
+    }
+
+    //span[contains(@data-bind,'.full_name')]     название продуктов;
+    //span[contains(@data-bind,'.minPrice')]      цена продуктов
+    //div[@class="schema-product__status"]  нет в наличии
+    //div[@class="schema-product__offers"] количество предложений
+    //div[contains(@class, "schema-product")]
+
+    public SelenideElement getSchemaHeadText() {
+        return schemaHeaderText;
+    }
 }
-//span[contains(@data-bind,'.minPrice')]
--- src/main/java/org/example/steps/ShoppingSteps.java
+++ src/main/java/org/example/steps/ShoppingSteps.java
@@ -3,14 +3,80 @@ package org.example.steps;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.example.pages.ShoppingPage;
+import org.example.utils.Utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.codeborne.selenide.Selenide.actions;
 
 @Log4j2
 public class ShoppingSteps {
     ShoppingPage shoppingPage = new ShoppingPage();
+    Utils utils = new Utils();
 
     @Step
     public String extractTextFromSchemaHeader() {
         log.info("//// Extract text from Schema Head: " + shoppingPage.getSchemaHeadText().getText());
-       return shoppingPage.getSchemaHeadText().getText();
+        return shoppingPage.getSchemaHeadText().getText();
+    }
+
+    @Step
+    public void closeControlElement() {
+        if (shoppingPage.controlElement.isDisplayed()) {
+            log.info("Found control element");
+            shoppingPage.controlButton.click();
+            log.info("Control element is closed");
+        }
+        log.info("Control element not found");
+    }
+
+    @Step
+    public void clickSchemaFilterCheckboxItem(String label, String checkBoxItemName) {
+        log.info("click checkBox Item: " + checkBoxItemName);
+        shoppingPage.schemaFilterCheckboxItem(label, checkBoxItemName).scrollTo();
+        actions().moveToElement(shoppingPage.schemaFilterCheckboxItem(label, checkBoxItemName))
+                .click(shoppingPage.schemaFilterCheckboxItem(label, checkBoxItemName)).perform();
+    }
+
+    @Step
+    public void enterMinValueOfLimit(String label, String minValue) {
+        log.info("enter minValue of " + label + ": " + minValue);
+        shoppingPage.minValueField(label).scrollTo();
+        actions().click(shoppingPage.minValueField(label)).sendKeys(minValue).perform();
+    }
+
+    @Step
+    public void enterMaxValueOfLimit(String label, String maxValue) {
+        log.info("enter maxValue " + label + ": " + maxValue);
+        shoppingPage.maxValueField(label).scrollTo();
+        actions().click(shoppingPage.maxValueField(label)).sendKeys(maxValue).perform();
+    }
+
+    @Step
+    public void clickSchemaFilterDropDown(String label) {
+        log.info("");
+        shoppingPage.schemaFilterDropDown(label).click();
+    }
+
+    @Step
+    public void clickPopoverVisibleItem(String label, String itemName) {
+        log.info("");
+        shoppingPage.popoverVisibleItem(label, itemName).click();
+    }
+
+    @Step
+    public int getNumberFromSchemaFilterButton() {
+        utils.sleep(3000);
+        int result = 0;
+        String text = shoppingPage.schemaFilterButton().getText();
+        log.info("Text from FilterButton: " + shoppingPage.schemaFilterButton().getText());
+        Pattern pattern = Pattern.compile("\\d+( \\d+)?");
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            result = Integer.parseInt(matcher.group().replace(" ", ""));
+        }
+        log.info("Found number phones: " + result);
+        return result;
     }
 }

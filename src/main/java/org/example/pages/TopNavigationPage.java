8cb852e0e8e54efc8109a057089f874642587cd1
Basil Akavity <vakavity@gmail.com>
2023-11-30 12:10:02 +0300

    cleaned code

--- src/main/java/org/example/pages/TopNavigationPage.java
+++ src/main/java/org/example/pages/TopNavigationPage.java
@@ -18,24 +18,9 @@ public class TopNavigationPage {
     public SelenideElement asideListItem(String dataId, String asideItemName) {
         return $(By.xpath("//div[@data-id='" + dataId + "']//div[contains(text(),'" + asideItemName + "')]"));
     }
-//    //div[@data-id='" + data_id + "']//div[contains(text(),'" + sectionOfNavigation + "')]
-    //div[contains(text(), '" + sectionName + "')]
 
     public SelenideElement dropDownItem(String dataId, String asideItemName, String dropDownItemName) {
         return $(By.xpath("//div[@data-id='" + dataId + "']//div[contains(text()," +
                 " '" + asideItemName + "')]//..//span[contains(text(), '" + dropDownItemName + "')]"));
     }
-
-    //div[contains(text(), 'sectionOfAsideList')]//..//a[contains(@href, 'sectionOfDropdownList')]
-    // //div[contains(text(), 'Мобил')]//..//a[contains(@href, 'headphones')]
-    //div[contains(text(), 'Мобил')]//..//span[contains(text(), 'Фитнес')]
-    //div[contains(@class, 'aside catalog')]//div[contains(text(), 'Ноутбуки')]
-
-
-
-    ////div[@data-id='1']//div[contains(text(), 'Моб')]//..//span[contains(text(), 'Фитнес-браслеты')]
-
-
-    ////div[contains(text(), '" + sectionOfAsideList + "')]//..//span[contains(text(), '"
-    //                + sectionOfDropdownList + "')]
 }

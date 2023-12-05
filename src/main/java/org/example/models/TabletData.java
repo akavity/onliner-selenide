49d87a65f381b26b12aef3f1d4ce3dce780b5a37
Basil Akavity <vakavity@gmail.com>
2023-12-05 21:49:41 +0300

    add tabletData.json

---
+++ src/main/java/org/example/models/TabletData.java
@@ -0,0 +1,26 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TabletData {
+    String topMenuItemName;
+    String classifierItemName;
+    String asideItemName;
+    String dropDownItemName;
+    String labelPrice;
+    String minPrice;
+    String maxPrice;
+    String labelManufacturer;
+    String manufacturerName;
+    String labelShop;
+    String shopName;
+    String labelDate;
+    String releaseDate;
+    String filter;
+    double priceOfTheCheapestTablet;
+}

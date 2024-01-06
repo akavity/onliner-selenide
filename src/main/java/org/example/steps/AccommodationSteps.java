e0ff57cd4af4b89e1f9ec6c1ef97bf6a54173e80
Basil Akavity <vakavity@gmail.com>
2024-01-06 12:32:57 +0300

    changed AccommodationSteps

--- src/main/java/org/example/steps/AccommodationSteps.java
+++ src/main/java/org/example/steps/AccommodationSteps.java
@@ -3,33 +3,36 @@ package org.example.steps;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.example.pages.AccommodationPage;
+import org.example.utils.Utils;
 
 @Log4j2
 public class AccommodationSteps {
     AccommodationPage accommodation = new AccommodationPage();
+    Utils utils = new Utils();
 
     @Step
     public int getPrice() {
-        int result = Integer.parseInt(accommodation.getPriceField().getText()
-                .replace("$", "")
-                .replace(" ", ""));
+        String text = accommodation.getPriceField().getText();
+        log.info("Get text: " + text);
+        int result = utils.extractIntFromText(text, "\\d*[ ]\\d{3}([ ]\\d{3})?");
         log.info("Accommodation price: " + result);
         return result;
     }
 
     @Step
     public int getNumberRooms() {
-        int number = Integer.parseInt(accommodation.getNumberRoomsField().getText()
-                .replace("-комнатная квартира", ""));
+        String text = accommodation.getNumberRoomsField().getText();
+        log.info("Get text: " + text);
+        int number = utils.extractIntFromText(text, "\\d+");
         log.info("Number rooms: " + number);
         return number;
     }
 
     @Step
     public double getArea() {
-        double area = Double.parseDouble(accommodation.getAreaField().getText()
-                .replace(" м","")
-                .replace(",","."));
+        String text = accommodation.getAreaField().getText();
+        log.info("Get text: " + text);
+        double area = utils.extractDoubleFromText(text, "\\d+,?\\d{1,2}");
         log.info("Area of accommodation: " + area);
         return area;
     }

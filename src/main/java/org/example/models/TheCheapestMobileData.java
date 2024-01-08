92312d38c8cb2aadbeb66100e81358df0c87e3b2
Basil Akavity <vakavity@gmail.com>
2024-01-08 22:25:32 +0300

    added theCheapestMobileData

---
+++ src/main/java/org/example/models/TheCheapestMobileData.java
@@ -0,0 +1,25 @@
+package org.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TheCheapestMobileData {
+    String topMenuItemName;
+    String classifierItemName;
+    String asideItemName;
+    String dropDownItemName;
+    String labelPrice;
+    String minPrice;
+    String maxPrice;
+    String labelManufacturer;
+    String manufacturerName;
+    String labelShop;
+    String shopName;
+    String labelDate;
+    String releaseDate;
+    String pieceOfName;
+}

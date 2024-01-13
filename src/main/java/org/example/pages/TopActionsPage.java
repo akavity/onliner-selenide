33d9b4a3c59221ee1c2c649166fa2b0c2337d5df
Basil Akavity <vakavity@gmail.com>
2024-01-13 23:25:10 +0300

    save

--- src/main/java/org/example/pages/TopActionsPage.java
+++ src/main/java/org/example/pages/TopActionsPage.java
@@ -1,14 +1,20 @@
 package org.example.pages;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class TopActionsPage {
-    private final SelenideElement fastSearchField = $(By.xpath("//form[contains(@class, 'fast-search')]"));
+    private final SelenideElement fastSearchField = $(By.xpath("//input[contains(@class, 'fast-search')]"));
     private final SelenideElement authButton = $(By.xpath("//div[contains(text(), 'Вход')]")); // remove logInButton in TopNavigation
     private final SelenideElement cartButton = $(By.xpath("//a[@title='Корзина']/.."));
+
+    // fast search modal
+    private final ElementsCollection productTitleFields = $$(By.xpath("//div[@class='product__title']/a"));
+    private final SelenideElement modalIframe = $(By.xpath("//iframe[contains(@class,'modal-iframe')]"));
 
     public SelenideElement getFastSearchField() {
         return fastSearchField;
@@ -20,5 +26,13 @@ public class TopActionsPage {
 
     public SelenideElement getCartButton() {
         return cartButton;
+    }
+
+    public ElementsCollection getProductTitleFields() {
+        return productTitleFields;
+    }
+
+    public SelenideElement getModalIframe() {
+        return modalIframe;
     }
 }

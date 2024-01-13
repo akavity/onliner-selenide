b8c97c614812b5325b4e59624b9745b173530706
Basil Akavity <vakavity@gmail.com>
2024-01-13 23:26:35 +0300

    used switchTo().frame

---
+++ src/main/java/org/example/steps/TopActionsSteps.java
@@ -0,0 +1,27 @@
+package org.example.steps;
+
+import lombok.extern.log4j.Log4j2;
+import org.example.pages.TopActionsPage;
+import org.example.utils.Utils;
+
+import static com.codeborne.selenide.Selenide.switchTo;
+
+@Log4j2
+public class TopActionsSteps {
+    TopActionsPage topActionsPage = new TopActionsPage();
+    Utils utils = new Utils();
+
+    public void enterProductName(String name) {
+        topActionsPage.getFastSearchField().click();
+        log.info("Enter the product name to search");
+        topActionsPage.getFastSearchField().sendKeys(name);
+        utils.sleep(3000);
+    }
+
+    public void clickFirstProduct() {
+        log.info("Switch to frame");
+        switchTo().frame(topActionsPage.getModalIframe());
+        log.info("Click first product");
+        topActionsPage.getProductTitleFields().first().click();
+    }
+}

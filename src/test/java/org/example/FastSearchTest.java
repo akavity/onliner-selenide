85e9eb495862507854aabd9d945e09437358a15a
Basil Akavity <vakavity@gmail.com>
2024-01-15 17:36:18 +0300

    added dataProvider

--- src/test/java/org/example/FastSearchTest.java
+++ src/test/java/org/example/FastSearchTest.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import org.example.annotations.TestData;
+import org.example.models.SearchData;
 import org.example.steps.CartSteps;
 import org.example.steps.ProductSteps;
 import org.example.steps.TopActionsSteps;
+import org.example.utils.JsonReaderGson;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,14 +14,17 @@ public class FastSearchTest extends BaseTest {
     ProductSteps productSteps = new ProductSteps();
     CartSteps cartSteps = new CartSteps();
 
-    @Test
-    public void addProductInCart() {
-        topActionsSteps.enterProductName("HONOR Pad X8 LTE");
+    @TestData(jsonFile = "searchData", model = "SearchData")
+    @Test(description = "Add product to cart",
+            dataProviderClass = JsonReaderGson.class, dataProvider = "getData")
+    public void addProductToCart(SearchData searchData) {
+        topActionsSteps.enterProductName(searchData.getProduct());
         topActionsSteps.clickFirstProduct();
+        productSteps.closeYourCityPopUpWindow();
         productSteps.addToCart();
         productSteps.clickRecommendedCartButton();
 
         String actual = cartSteps.extractTextFromProductTitle();
-        Assert.assertTrue(actual.contains("HONOR Pad X8 LTE"));
+        Assert.assertTrue(actual.contains(searchData.getProduct()));
     }
 }

c131c7bb616deafd354e67807b5cad26a1638b0b
Basil Akavity <vakavity@gmail.com>
2024-01-15 17:34:25 +0300

    added new Steps

--- src/main/java/org/example/steps/ProductSteps.java
+++ src/main/java/org/example/steps/ProductSteps.java
@@ -3,10 +3,14 @@ package org.example.steps;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.example.pages.ProductPage;
+import org.example.pages.TopActionsPage;
+import org.example.utils.Utils;
 
 @Log4j2
 public class ProductSteps {
     ProductPage productPage = new ProductPage();
+    TopActionsPage topActionsPage = new TopActionsPage();
+    Utils utils = new Utils();
 
     @Step
     public String extractTextFromProductHeader() {
@@ -22,8 +26,23 @@ public class ProductSteps {
     }
 
     @Step
+    public void closeYourCityPopUpWindow() {
+        if (productPage.getYourCityPopUpWindow().isDisplayed()) {
+            log.info("Closed your city pop-up window");
+            productPage.getYesCityButton().click();
+        }
+    }
+
+    @Step
     public void clickRecommendedCartButton() {
         log.info("Click recommended cart button");
-        productPage.getRecommendedCartButton().click();
+        utils.sleep(1000);
+        if (productPage.getRecommendedSidebarAside().isDisplayed()) {
+            log.info("Recommended sidebar aside is displayed");
+            productPage.getRecommendedCartButton().click();
+        } else {
+            log.info("Recommended sidebar aside isn't displayed");
+            topActionsPage.getCartButton().click();
+        }
     }
 }
